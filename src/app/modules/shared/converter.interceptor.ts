deb747ae65bbbab252f2bd2d9835e8fed8b320f4
Kostia Lubinskiy <kostia.lub@mail.ru>
2020-11-05 15:25:53 +0300

    remove success message, change position of message

--- src/app/modules/shared/converter.interceptor.ts
+++ src/app/modules/shared/converter.interceptor.ts
@@ -12,24 +12,19 @@ export class ConverterInterceptor implements HttpInterceptor {
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-
     return next.handle(req).pipe(
       tap(evt => {
         if (evt instanceof HttpResponse) {
-          if (evt.body.length > 0) {
-            this.toasterService.success('Данные загружены', 'Готово', { positionClass: 'toast-center-center' });
-          };
           if (evt.body.length === 0) {
-            this.toasterService.warning('На выбранную дату данных нет', 'Ошибка', { positionClass: 'toast-center-center' });
+            this.toasterService.warning('На выбранную дату данных нет', 'Ошибка', { positionClass: 'toast-bottom-right' });
           };
         }
       }),
       catchError((error: any) => {
         if (error instanceof HttpErrorResponse) {
-          this.toasterService.error('Сервер не отвечает', 'Ошибка', { positionClass: 'toast-center-center' });
+          this.toasterService.error('Сервер не отвечает', 'Ошибка', { positionClass: 'toast-bottom-right' });
         }
         return of(error);
       }));
-
   }
 }
