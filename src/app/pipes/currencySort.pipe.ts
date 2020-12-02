fd1f8cfa1995c35509cff30801fe43435f445cf2
Kostia Lubinskiy <kostia.lub@mail.ru>
2020-12-02 11:58:18 +0300

    fix sort

--- src/app/pipes/currencySort.pipe.ts
+++ src/app/pipes/currencySort.pipe.ts
@@ -9,26 +9,22 @@ export type SortOrder = 'asc' | 'desc';
 })
 
 export class CurrencySortPipe implements PipeTransform {
-  transform(value: any[], sortOrder: SortOrder | string = 'asc', sortKey?: string): any {
+  transform(value: any[], sortOrder: SortOrder | string = 'asc', sortKey?: string, rate?: string): any {
     sortOrder = sortOrder && (sortOrder.toLowerCase() as any);
+    console.log(value, sortOrder, sortKey)
 
     if (!value || (sortOrder !== 'asc' && sortOrder !== 'desc')) return value;
 
     let numberArray = [];
     let stringArray = [];
 
-    if (!sortKey) {
-      numberArray = value.filter(item => typeof item === 'number').sort();
-      stringArray = value.filter(item => typeof item === 'string').sort();
-    } else {
-      numberArray = value.filter(item => typeof item[sortKey] === 'number').sort((a, b) => a[sortKey] - b[sortKey]);
-      stringArray = value
-        .filter(item => typeof item[sortKey] === 'string')
-        .sort((a, b) => {
-          if (a[sortKey] < b[sortKey]) return -1;
-          else if (a[sortKey] > b[sortKey]) return 1;
-          else return 0;
-        });
+    if (sortKey) {
+      numberArray = value.filter(item => typeof item[sortKey] === 'number').sort((a, b) => a[sortKey]/a[rate] - b[sortKey]/b[rate]);
+      stringArray = value.filter(item => typeof item[sortKey] === 'string').sort((a, b) => {
+        if (a[sortKey] < b[sortKey]) return -1;
+        else if (a[sortKey] > b[sortKey]) return 1;
+        else return 0;
+      });
     }
     const sorted = numberArray.concat(stringArray);
     return sortOrder === 'asc' ? sorted : sorted.reverse();
